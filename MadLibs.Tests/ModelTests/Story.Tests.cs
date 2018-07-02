b0a235b838e256fa415024d760e3118247415405
Elly Maimon and Thad Donaghue <elly@>
2018-07-02 08:25:07 -0700

    initial commit

---
+++ MadLibs.Tests/ModelTests/Story.Tests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using MadLibs;
+namespace MadLibs.Tests
+{
+    [TestClass]
+    public class StoryTest
+    {
+        [TestMethod]
+        public void Name()
+        {
+
+        }
+    }
+}
---
+++ MadLibs/Models/Story.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace MadLibs
+{
+    public class Story
+    {
+
+    }
+}
